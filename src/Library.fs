153a8af09834c69057e1f2eaa70568815ec205f1
gretmn102 <erlgreywatson@gmail.com>
2026-09-11 23:32:04 +0300

    refactor(Library.fs): extract `Ffprob` to file

--- src/Library.fs
+++ src/Library.fs
@@ -147,34 +147,3 @@ module FfMpeg =
                 yield! Options.toArgs options
                 $"\"{output}\""
             ])
-
-module Ffprobe =
-    let ffprobPath = "ffprobe"
-
-    let startProc args =
-        printfn "%s %s" ffprobPath args
-        Proc.startProcString ffprobPath args
-
-    let getTimebase inputPath =
-        let args =
-            String.concat " " [
-                "-v error"
-                "-select_streams v:0"
-                "-show_entries stream=time_base"
-                "-of default=noprint_wrappers=1:nokey=1"
-                Bash.toDoubleQuote inputPath
-            ]
-        let exitCode, stdout, stderr = startProc args
-        exitCode, stdout.Trim(), stderr
-
-    let getFps inputPath =
-        let args =
-            String.concat " " [
-                "-v error"
-                "-select_streams v:0"
-                "-show_entries stream=r_frame_rate"
-                "-of csv=p=0"
-                Bash.toDoubleQuote inputPath
-            ]
-        let exitCode, stdout, stderr = startProc args
-        exitCode, stdout.Trim(), stderr
